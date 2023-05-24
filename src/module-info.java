f59dceb1e5de1b29b68bd8550008c7853b50820f
Unknown <fmenke@stud.hs-bremen.de>
2023-05-24 22:18:20 +0200

    Test um 22:18

---
+++ src/module-info.java
@@ -0,0 +1,88 @@
+module Lemminge {
+}
+# Wesentliche Punkte f�r die Bewertung des Projekts
+
+- Vollst�ndigkeit und Korrektheit: Projektcode deckt die m�glichen Fehlerquellen ab, Projektcode implementiert die definierten Funktionen
+- Termintreue und Kommunikation: Kommunikation innerhalb des Projekts, Termintreue, Nutzung der Email-Tags usw.
+- Projekt- und Code-Struktur: siehe Abschnitt unten
+- Kommentare und Nachvollziehbarkeit: siehe Abschnitt unten
+- Komplexit�t: siehe Abschnitt unten
+- Anwendungswissen: Anwendung der Inhalte aus der Vorlesung, Pr�sentation von Programmierkenntnissen
+- �bertragungswissen: Pr�sentieren von Kenntnissen �ber die Vorlesung hinaus
+- Dokumentation: Bewertung der Projektdokumentation (Projektbericht)
+- Gesamteindruck: Gesamteindruck des Projekts
+- Pr�sentation: Benotung der Abschlusspr�sentation
+- Individualwissen: Einzelnote aus dem Kolloquium
+
+je Kriterium bis zu zehn Punkte: $\sum = 110 \text{Punkte}$
+
+## Vollst�ndigkeit/Korrektheit
+
+- Basisfunktionalit�t des Programms gegeben: Happy-Path-Szenario
+- keine undokumentierte Bugs, Fehlerquellen, Abst�rze usw.
+- geforderte Funktionen:
+  - Umsetzung des Basisspiels (auftauchen, anklicken der Lemminge)
+  - Auswerten der Spielerleistung
+  - Speichern eines Highscores (textbasiert, CSV o.�.)
+
+## Termintreue/Kommunikation
+
+- rechtzeitige Abgaben (Termintreue)
+- Erscheinen zur Projektbesprechung (oder entsprechende rechtzeitige Abmeldung)
+- klare Email-Kommunikation (Verwendung der Email-Tags)
+
+## Projekt- und Code-Struktur
+
+- klare Umsetzung der Objektorientierung:
+  - sinnvolle Darstellung des Problems (Model) in Klassen, evtl. Vererbung usw.
+  - wiederverwendbare Klassen mit klaren Schnittstellen
+  - sinnvolle Datentypen und Attributeigenschaften (final, static, public, private usw.)
+- sinnvolle Strukturierung des Projekts (Packages, Klassenfunktionen etc)
+- klare, strikte Umsetzung von MVC (Trennung von GUI & Model):
+  - Modelcode ist **nur** im Model zu finden
+  - GUI-Funktionen (Fehlermeldungen, Interaktionen, Darstellung des Models) ist vom Model getrennt
+
+## Kommentare & Nachvollziehbarkeit
+
+- sinnvolle Kommentare, nachvollziehbarer Code
+- Java-Programmierstil (Variablennamen)
+- formatierter Programmcode (nach Eclipse-Stil)
+- DRY-coding (don't repeat yourself)
+- (optional) Javadoc Dokumentation
+
+## Komplexit�t
+
+- der Vorlesung angemessene Komplexit�t des Programms (siehe Beispielprogramme)
+- interaktive Benutzeroberflaeche (keine GUI-Builder, oder Engines!)
+- Codeumfang der Gruppengr�sse angemessen
+
+## Anwendungswissen
+
+- Verwendung der in der Vorlesung/Tutorium vorgestellten Sprachfunktionen von Java
+- Pr�sentation von Anwendungswissen: Verzweigungen, Schleifen, usw.
+
+## �bertragungswissen
+
+- �ber Vorlesungsinhalte hinausgehende Sprachkenntnisse
+- fortgeschrittene Algorithmen und deren klare Umsetzung
+
+## Dokumentation
+
+- Dokumentation des Projektplans, Beschreibung der Problemstellung
+- Diskussion der Kernprobleme des Projekts, Realisierung derselben
+- eventuell Fehlerdiskussion und Korrekturm�glichkeiten
+- Ausblick, Erweiterungsm�glichkeiten des Projekts
+- Fazit
+- graphische Darstellung (Abbildungen)
+- Formatierung (LaTeX wird pr�feriert)
+- Rechtschreibung, Grammatik, Umgang mit Quellen
+
+## Pr�sentation
+
+- Projektdarstellung
+- Eingesetzte Mittel
+- Kernproblem
+- Schwierigkeiten
+- Abstraktion
+- Softwaredemo
+- Gesamteindruck
